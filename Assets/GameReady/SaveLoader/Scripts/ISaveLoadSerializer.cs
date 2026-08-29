18b452076325c7b9973ac18031bd5e0da3f5c95a
hyunwoo <davidkimighty@gmail.com>
2026-08-29 11:11:06 +0900

    cleanup

--- Assets/GameReady/SaveLoader/Scripts/ISaveLoadSerializer.cs
+++
@@ -1,8 +0,0 @@
-namespace GameReady.SaveLoad
-{
-    public interface ISaveLoadSerializer
-    {
-        string Serialize(object data);
-        void Deserialize(string serializedData, object target);
-    }
-}
--- Assets/GameReady/SaveLoader/Scripts/ISaveable.cs
+++
@@ -1,8 +0,0 @@
-namespace GameReady.SaveLoad
-{
-    public interface ISaveable
-    {
-        void SaveState(object state);
-        void LoadState(object state);
-    }
-}
